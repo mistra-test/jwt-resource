3081c4060ac147e19e9db7ccf79d45a416228665
Mistra <sperectrum@gmail.com>
2021-06-22 01:55:15 +0200

    token duration is now 2 hours

--- src/main/java/com/example/jwtresource/service/JwtService.java
+++ src/main/java/com/example/jwtresource/service/JwtService.java
@@ -71,7 +71,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 7200))
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
