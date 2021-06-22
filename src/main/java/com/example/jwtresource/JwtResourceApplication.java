88fa7bfc75dd2abc8215ace56948e6707e3fce1c
Mistra <sperectrum@gmail.com>
2021-06-22 23:34:02 +0200

    native hints and native build

--- src/main/java/com/example/jwtresource/JwtResourceApplication.java
+++ src/main/java/com/example/jwtresource/JwtResourceApplication.java
@@ -1,8 +1,14 @@
 package com.example.jwtresource;
 
+import io.jsonwebtoken.impl.DefaultJwtBuilder;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
+@TypeHint(types = MacProvider.class)
+@TypeHint(types = DefaultJwtBuilder.class)
 @SpringBootApplication
 public class JwtResourceApplication {
 
